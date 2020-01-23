6296938fbe88cdf1774231705b47b65987fbe725
hexiaolei <1035271660@qq.com>
2020-01-23 14:03:57 +0800

    add loigin support

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const fetch = require('node-fetch');
+const axios = require('axios');
 const { APP_SECRET, APP_ID } = require('../config');
 
 /* GET users listing. */
@@ -9,14 +9,19 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/login', function (req, res, next) {
-    const code = req.code;
-    fetch("https://api.weixin.qq.com/sns/jscode2session", {
-        appid: APP_ID,
-        secret: APP_SECRET,
-        js_code: code,
-        grant_type: "authorization_code"
+    const code = req.body.code;
+    axios.get("https://api.weixin.qq.com/sns/jscode2session", {
+        params: {
+            appid: APP_ID,
+            secret: APP_SECRET,
+            js_code: code,
+            grant_type: "authorization_code"
+        }
     }).then((result) => {
-        console.log(result);
+        console.log(result.data);
+        res.send('error');
+    }, err => {
+        console.error(11, err);
     });
 });
 
